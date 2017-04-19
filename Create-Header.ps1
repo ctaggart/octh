78683b6500a547dca60e8850c3c1fe6aa0130377
Cameron Taggart <cameron@tachyus.com>
2017-04-18 22:18:07 -0500

    fix #2 do not include x86intrin.h

--- Create-Header.ps1
+++ Create-Header.ps1
@@ -1,10 +1,8 @@
 $env:PATH = "C:\Octave\Octave-4.2.1\bin;$env:PATH"
 & gcc -E -v `
 -x c++ `
--std=gnu++11 `
+-std=c++11 `
 -IC:\Octave\Octave-4.2.1\include\octave-4.2.1\octave `
 -IC:\Octave\Octave-4.2.1\include `
--IC:\Octave\Octave-4.2.1\lib\gcc\x86_64-w64-mingw32\4.9.4\include\c++ `
--IC:\Octave\Octave-4.2.1\lib\gcc\x86_64-w64-mingw32\4.9.4\include\c++\x86_64-w64-mingw32 `
 -o bindgen2.h `
 bindgen.h
--- bindgen.h
+++ bindgen.h
@@ -1,1 +1,2 @@
+#define _X86INTRIN_H_INCLUDED
 #include "oct.h"
