9bd7c29248de4262a364332ce73717d4475524cb
Cameron Taggart <cameron@tachyus.com>
2017-04-20 09:24:08 -0500

    started working on bindgen in build.rs Create-Header works with clang instead of gcc build.rs bindgen() not picking up mingw includes

--- build.rs
+++ build.rs
@@ -1,2 +1,26 @@
-fn main () {
+extern crate bindgen;
+
+fn bindgen() {
+    let bindings = bindgen::Builder::default()
+        .no_unstable_rust()
+        .header("bindgen.h")
+        .clang_arg("-x")
+        .clang_arg("c++")
+        .clang_arg("-std=c++11")
+        .clang_arg("-target")
+        .clang_arg("x86_64-w64-mingw32")
+        .clang_arg("-IC:/Octave/Octave-4.2.1/include/octave-4.2.1/octave")
+        // .enable_cxx_namespaces()
+        // .opaque_type(".*")
+        // .whitelisted_type("octave_.*")
+        .generate()
+        .expect("Unable to generate bindings");
+
+    bindings
+        .write_to_file("src/lib.rs")
+        .expect("Couldn't write bindings!");
+}
+
+fn main() {
+    bindgen();
 }
