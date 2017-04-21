8a884a46ce96e4a6395ecdb028ebef97835ccf0a
Cameron Taggart <cameron@tachyus.com>
2017-04-20 19:48:14 -0500

    build.rs able to create lib.rs lib.rs not yet compiling

--- Create-Header.ps1
+++
@@ -1,8 +0,0 @@
-$env:PATH = "C:\Octave\Octave-4.2.1\bin;$env:PATH"
-& clang -E -v `
--x c++ `
--std=c++11 `
--target x86_64-w64-mingw32 `
--IC:\Octave\Octave-4.2.1\include\octave-4.2.1\octave `
--o bindgen2.h `
-bindgen.h
