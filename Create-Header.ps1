19a82e296062db3f058d433323dc06b9685756ed
Cameron Taggart <cameron.taggart@gmail.com>
2017-04-20 19:49:46 -0500

    bindgen in build.rs able to create lib.rs (#6)
    * started working on bindgen in build.rs
    Create-Header works with clang instead of gcc
    build.rs bindgen() not picking up mingw includes
    
    * build.rs able to create lib.rs
    lib.rs not yet compiling

--- Create-Header.ps1
+++
@@ -1,8 +0,0 @@
-$env:PATH = "C:\Octave\Octave-4.2.1\bin;$env:PATH"
-& gcc -E -v `
--x c++ `
--std=c++11 `
--IC:\Octave\Octave-4.2.1\include\octave-4.2.1\octave `
--IC:\Octave\Octave-4.2.1\include `
--o bindgen2.h `
-bindgen.h
