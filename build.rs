e083a722898d9b363ef2b2b9a76848a06e42e91b
Cameron Taggart <cameron.taggart@gmail.com>
2017-05-18 20:24:30 +0200

    able to build lib.rs (#19)
    * try making types opaque
    got past #8, but ran into #11
    
    * it compiles
    
    * genereated with latest bindgen

--- build.rs
+++ build.rs
@@ -1,9 +1,10 @@
 extern crate bindgen;
 
 fn bindgen() {
-    let bindings = bindgen::Builder::default()
+    let mut builder = bindgen::Builder::default()
         .no_unstable_rust()
         .header("bindgen.h")
+        // .header("_mingw_mac.h")
         .clang_arg("-v")
         .clang_arg("-x")
         .clang_arg("c++")
@@ -12,17 +13,41 @@ fn bindgen() {
         .clang_arg("-nobuiltininc")
         .clang_arg("-IC:/Octave/Octave-4.2.1/include/octave-4.2.1/octave")
         .enable_cxx_namespaces()
-        // .opaque_type(".*")
-        .whitelisted_type("octave_.*")
-        .generate_comments(false)
-        .generate()
+        .opaque_type(".*") // Should have a set of used template params for every item id
+        // .whitelisted_type("octave_.*")
+        .layout_tests(false)
+        .generate_comments(false);
+
+        // let whitelist = [
+        //     "octave_value_list"
+        // ];
+        // for &ty in whitelist.iter() {
+        //     builder = builder.whitelisted_type(ty);
+        // }
+
+        // let opaque_types = [
+        //     "std::basic_streambuf___streambuf_type",
+        //     "std::basic_istream_sentry_traits_type",
+        //     "std::basic_streambuf" // std::basic_streambuf<_CharT>
+        // ];
+        // for &ty in opaque_types.iter() {
+        //     builder = builder.opaque_type(ty);
+        // }
+
+        // let blacklist = [
+        //     "_CType"
+        // ];
+        // for &ty in blacklist.iter() {
+        //     builder = builder.hide_type(ty);
+        // }
+
+    let bindings = builder.generate()
         .expect("Unable to generate bindings");
 
-    bindings
-        .write_to_file("src/lib.rs")
+    bindings.write_to_file("src/lib.rs")
         .expect("Couldn't write bindings!");
 }
 
 fn main() {
-    bindgen();
+    // bindgen();
 }
