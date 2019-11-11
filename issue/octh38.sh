ea15d9c838c9bbaa11b8d80d9d7926aa1b54217e
Cameron Taggart <cameron.taggart@gmail.com>
2019-11-11 14:09:59 +0000

    down to 2491 lines

--- issue/octh38.sh
+++ issue/octh38.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # time creduce ./issue/octh38.sh __bindgen.ii
 
-cp /home/cameron/github/octh/Cargo.toml .
-cp /home/cameron/github/octh/build.rs .
+cp $HOME/github/octh/Cargo.toml .
+cp $HOME/github/octh/build.rs .
 mkdir src
-cp /home/cameron/github/octh/src/lib.rs src
-cp /home/cameron/github/octh/src/octhelp.cc src
-cp /home/cameron/github/octh/src/octhelp.h src
+cp $HOME/github/octh/src/lib.rs src
+cp $HOME/github/octh/src/octhelp.cc src
+cp $HOME/github/octh/src/octhelp.h src
 BINDGEN_HEADER="__bindgen.ii" cargo test \
 2>&1 \
 | grep "thread 'root::octave::bindgen_test_layout_output_system' panicked at 'assertion failed:"
